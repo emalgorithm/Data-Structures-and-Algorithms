a18dbd025769087f9205264e769857dcf1f9facf
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2015-10-25 15:27:10 +0000

    Implemented Square root decomposition solution for Lowest Common Ancestor problem

---
+++ Graph's Algorithms/Lowest Common Ancestor/LCA-squareRootDecomposition.cpp
@@ -0,0 +1,72 @@
+//This algorithm allows you to find the Lowest Common Ancestor of two nodes in
+//O(N) preprocessing and O(sqrt(N) per query using a technique called
+//'Square root decomposition'
+//After having construct the tree in an adjacency list, it is needed to call
+//the two first function for the preprocessing in this way:
+//findParantsAndLevel(root, root);
+//findAncestor(root, root, sqrt(height));
+//After that it is possible to call the function LCA
+
+const int MAX_N = 100000;
+
+vector < int > tree[MAX_N];
+bool visited[MAX_N] = {false};
+int ancestor[MAX_N];
+int level[MAX_N];
+int parent[MAX_N];
+int height = 0;
+
+void findParentsAndLevel(int node, int root) {
+    visited[node] = true;
+    if(node == root) {
+        level[node] = 0;
+        parent[node] = node;
+    }
+    for(int i = 0; i < tree[node].size(); i++) {
+        int son = tree[node][i];
+        if(visited[son] == false) {
+            level[son] = level[node] + 1;
+            parent[son] = node;
+            height = max(height, level[son] + 1);
+            findParentsAndLevel(son, root);
+        }
+    }
+}
+
+void findAncestor(int node, int root, int heightSqrt) {
+    if(level[node] < heightSqrt) {
+        ancestor[node] = root;
+    }
+    else {
+        if(level[node] % heightSqrt == 0) { //The node is in the first layer
+            ancestor[node] = parent[node];
+        }
+        else {
+            ancestor[node] = ancestor[parent[node]];
+        }
+    }
+    for(int i = 0; i < tree[node].size(); i++) {
+        int son = tree[node][i];
+        if(visited[son] == 0) {
+            findAncestor(son, root, heightSqrt);
+        }
+    }
+}
+
+int LCA(int firstNode, int secondNode) {
+    if(level[firstNode] > level[secondNode]) {
+        swap(firstNode, secondNode);
+    }
+    while(ancestor[firstNode] != ancestor[secondNode]) {
+        secondNode = ancestor[secondNode];
+    }
+    while(firstNode != secondNode) {
+        if(level[firstNode] > level[secondNode]) {
+            firstNode = parent[firstNode];
+        }
+        else {
+            secondNode = parent[secondNode];
+        }
+    }
+    return firstNode;
+}
